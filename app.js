8e25e14835965b0051bfbe011a13a1078e51aa8c
Santosh Kumar <santoshkumar964887@gmail.com>
2020-07-08 02:42:56 +0530

    refactoring our routes

--- app.js
+++ app.js
@@ -1,105 +1,96 @@
-const express=require('express');
-const fs=require('fs');
-const app=express();
+const express = require("express");
+const fs = require("fs");
+const app = express();
 app.use(express.json());
-const tours=JSON.parse(fs.readFileSync(`${__dirname}/starter/dev-data/data/tours.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/starter/dev-data/data/tours.json`)
+);
 
-
-app.get('/api/v1/tours',(req,res)=>{
+const getAlltour=(req, res) => {
     res.status(200).json({
-        "status":'success',
-        'length':tours.length,
-        'data':{
-            tours
-        }
+      status: "success",
+      length: tours.length,
+      data: {
+        tours,
+      },
     });
-
-});
-
-
-app.get('/api/v1/tours/:id',(req,res)=>{
-    console.log(req.prams)
-    const id =req.params.id*1;
-    const match=tours.find(el=>el.id===id);
-    if(!match){
-        res.status(404).json({
-            "status":'Fail',
-            "message":'Invalid Id'
-        });
-
+  };
+const getOnetour=(req, res) => {
+    console.log(req.prams);
+    const id = req.params.id * 1;
+    const match = tours.find((el) => el.id === id);
+    if (!match) {
+      res.status(404).json({
+        status: "Fail",
+        message: "Invalid Id",
+      });
     }
     res.status(200).json({
-        "status":'success',
-        'length':tours.length,
-        'data':{
-            match
-        }
+      status: "success",
+      length: tours.length,
+      data: {
+        match,
+      },
     });
-
-});
-
-app.patch('/api/v1/tours/:id',(req,res)=>{
-    console.log(req.prams)
-    const id =req.params.id*1;
-   if(tours.length<id){
-       res.status(404).json({
-    "status":'Fail',
-    "message":'Invalid Id'
-});
-
-}
-res.status(200).json({
-"status":'success'
-});
-
-   
-});
-
-
-app.delete('/api/v1/tours/:id',(req,res)=>{
-    console.log(req.prams)
-    const id =req.params.id*1;
-   if(tours.length<id){
-       res.status(404).json({
-    "status":'Fail',
-    "message":'Invalid Id'
-});
-
-}
-res.status(200).json({
-"status":'success'
-});
-
-   
-});
-
-
-
-
-
-app.post('/api/v1/tours',(req,res)=>{
+  };
+const createNewtour=(req, res) => {
     //console.log(req.body);
-    let reqdata=req.body;
-    let newId=(tours.length)+1;
-    const newTour=Object.assign({id:newId},reqdata);
+    let reqdata = req.body;
+    let newId = tours.length + 1;
+    const newTour = Object.assign({ id: newId }, reqdata);
     tours.push(newTour);
-    const write=fs.writeFile(`${__dirname}/starter/dev-data/data/tours.json`,JSON.stringify(tours),(err)=>{
+    const write = fs.writeFile(
+      `${__dirname}/starter/dev-data/data/tours.json`,
+      JSON.stringify(tours),
+      (err) => {
         console.log(err);
-    })
-
+      }
+    );
+  
     res.status(201).json({
-        "status":'success',
-        "data":{
-            "tour":newTour
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  };
+const updatetour=(req, res) => {
+    console.log(req.prams);
+    const id = req.params.id * 1;
+    if (tours.length < id) {
+      res.status(404).json({
+        status: "Fail",
+        message: "Invalid Id",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+    });
+  };
+const deleteOnetour=(req, res) => {
+    console.log(req.prams);
+    const id = req.params.id * 1;
+    if (tours.length < id) {
+      res.status(404).json({
+        status: "Fail",
+        message: "Invalid Id",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+    });
+  };
 
-        }
+//app.get("/api/v1/tours", getAlltour);
+//app.get("/api/v1/tours/:id", getOnetour);
+//app.post("/api/v1/tours",createNewtour );
+//app.patch("/api/v1/tours/:id",updatetour );
+//app.delete("/api/v1/tours/:id", deleteOnetour);
+app.route("/api/v1/tours").get(getAlltour).post(createNewtour);
+app.route("/api/v1/tours/:id").get(getOnetour).patch(updatetour).delete(deleteOnetour);
 
-    })
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("App is running on port number 3000 ");
 });
-
-
-const port=3000;
-app.listen(port,()=>{
-    console.log('App is running on port number 3000 ')
-
-})
