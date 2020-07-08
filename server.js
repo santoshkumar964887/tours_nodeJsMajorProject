eb0ea5463903ef02f5fc8c7cf415edbb817c848e
Santosh Kumar <santoshkumar964887@gmail.com>
2020-07-09 03:00:05 +0530

    creting simple model and schema and inserted into database

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const app = require("./app");
 const dotenv=require('dotenv');
 const mongooes=require('mongoose');
 dotenv.config({path: './config.env'});
-console.log(process.env);
 mongooes.connect(process.env.DATABASE,{
   useNewUrlParser:true,
   useCreateIndex:true,
@@ -10,6 +9,26 @@ mongooes.connect(process.env.DATABASE,{
 }).then(con=>{
   console.log('data base connected');
 })
+
+const tourSchema= mongooes.Schema({
+  name:{
+    type:String,
+    required:[true,'name must be present'],
+    unique:true
+  },
+  age:{
+    type:Number,
+    default: 19
+  }
+});
+
+const Tour=mongooes.model('Tour',tourSchema);
+ const testtour= new Tour({
+   name:'santosh',
+   age:20
+ });
+ testtour.save();
+ 
 const port = 3000;
 app.listen(port, () => {
   console.log("App is running on port number 3000 ");
