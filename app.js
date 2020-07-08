61043974d3c3a04920272f1c0ad182bf71f2b78c
Santosh Kumar <santoshkumar964887@gmail.com>
2020-07-08 18:27:54 +0530

    adding .env file configruation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const tourRouter = require("./Router/toursRouter");
 const userRuter = require("./Router/userRouters");
 const app = express();
 app.use(express.json());
+app.use(express.static(`${__dirname}/starter/public`));
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRuter);
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const app = require("./app");
+const dotenv=require('dotenv');
+dotenv.config({path: './config.env'});
+console.log(process.env);
 const port = 3000;
 app.listen(port, () => {
   console.log("App is running on port number 3000 ");
