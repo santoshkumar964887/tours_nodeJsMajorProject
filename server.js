02922a1faba4251e0e8ac5290473e6d667853025
Santosh Kumar <santoshkumar964887@gmail.com>
2020-07-09 02:21:20 +0530

    connecting with data base and insatalled mongooes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const app = require("./app");
 const dotenv=require('dotenv');
+const mongooes=require('mongoose');
 dotenv.config({path: './config.env'});
 console.log(process.env);
+mongooes.connect(process.env.DATABASE,{
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  useFindAndModify:false
+}).then(con=>{
+  console.log('data base connected');
+})
 const port = 3000;
 app.listen(port, () => {
   console.log("App is running on port number 3000 ");
