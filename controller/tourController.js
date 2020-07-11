bd0c12dacef78976e941c67101da1906b6256463
Santosh Kumar <santoshkumar964887@gmail.com>
2020-07-12 03:05:26 +0530

    adding diffrent features in our api like sorting,limiting Fields,pagination

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,4 +1,5 @@
 const tourModel = require("./../model/tourmodel");
+const { Error } = require("mongoose");
 
 /*exports.checkReqbody=(req,res,next)=>{
     const {name}=req.body;
@@ -26,49 +27,72 @@ const tourModel = require("./../model/tourmodel");
   };*/
 exports.getAlltour = async (req, res) => {
   try {
-    const queryobj={...req.query};
-    const excludefields=['page','sort','limit','fields'];
+    const queryobj = { ...req.query };
+    //filter
+    const excludefields = ["page", "sort", "limit", "fields"];
+ 
+    excludefields.forEach((el) => delete queryobj[el]);
+    //Advanced filtering
+    let queryString = JSON.stringify(queryobj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+    let query = tourModel.find(JSON.parse(queryString));
+// sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      this.query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+// limitFields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+    //paginate
+    const page=req.query.page*1||1;
+    const limit=req.query.limit*1;
+    const skip=(page-1)*limit+1;
+    query=query.skip(skip).limit(limit);
     
-    excludefields.forEach(el=> delete queryobj[el]);
-    let queryString=JSON.stringify(queryobj);
-    queryString= queryString.replace(/\b(gte|gt|lte|lt)\b/g,match=>`$${match}`);
-    const tour = await tourModel.find(JSON.parse(queryString));
+
+    const tour = await query;
     res.status(200).json({
       status: "success",
       length: tour.length,
       data: {
-        tour
-      }
+        tour,
+      },
     });
   } catch (err) {
     res.status(404).json({
       status: "fail",
 
-      message: err
+      message: err,
     });
   }
 };
 exports.getOnetour = async (req, res) => {
-  
-   
   try {
-    const tour =await tourModel.findById(req.params.id);
+    const tour = await tourModel.findById(req.params.id);
     res.status(200).json({
       status: "success",
       length: tour.length,
       data: {
-        tour
-      }
+        tour,
+      },
     });
   } catch (err) {
     res.status(404).json({
       status: "fail",
 
-      message: err
+      message: err,
     });
   }
-  
-  
 };
 exports.createNewtour = async (req, res) => {
   //console.log(req.body);
@@ -79,8 +103,8 @@ exports.createNewtour = async (req, res) => {
     res.status(201).json({
       status: "success",
       data: {
-        data
-      }
+        data,
+      },
     });
   } catch (err) {
     res.status(400).json({
@@ -89,40 +113,39 @@ exports.createNewtour = async (req, res) => {
     });
   }
 };
-exports.updatetour = async(req, res) => {
+exports.updatetour = async (req, res) => {
   try {
-  const tour= await tourModel.findByIdAndUpdate(req.params.id,req.body,{
-    new:true
-  });
-  
+    const tour = await tourModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
     res.status(200).json({
       status: "success",
       length: tour.length,
       data: {
-        tour
-      }
+        tour,
+      },
     });
   } catch (err) {
     res.status(404).json({
       status: "fail",
 
-      message: err
+      message: err,
     });
   }
- 
 };
 exports.deleteOnetour = async (req, res) => {
   try {
-    const tour= await tourModel.findByIdAndDelete(req.params.id);
-    
-      res.status(200).json({
-        status: "success"
-      });
-    } catch (err) {
-      res.status(404).json({
-        status: "fail",
-  
-        message: err
-      });
-    }
+    const tour = await tourModel.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+
+      message: err,
+    });
+  }
 };
