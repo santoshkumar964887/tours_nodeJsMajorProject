277e7529b3b27eb4dd5ccd3a0c6da98bcfa7f771
Santosh Kumar <santoshkumar964887@gmail.com>
2020-07-14 06:03:40 +0530

    handling unhandled or wrong routes

--- app.js
+++ app.js
@@ -8,5 +8,11 @@ app.use(express.static(`${__dirname}/starter/public`));
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRuter);
+app.all('*',(req,res,next)=>{
+    res.status(404).json({
+        status:'Fail',
+        message: `can't find ${req.originalUrl} on this server`
+    });
+});
 
 module.exports = app;
